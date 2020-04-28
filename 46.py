b1c32b7d47a43aec16118415e390af1b2105be63
mygit20031984 <ashuklkrn@gmail.com>
2020-04-28 11:23:54 +0530

    changes in 46.py

--- 46.py
+++ 46.py
@@ -1,9 +1,21 @@
-import string
+#import string
 
-a = []
-for letter in string.ascii_lowercase:
-    f = open("Letters/" + letter+".txt", "r")
-    a.append(f.read())
-    f.close()
+#a = []
+#for letter in string.ascii_lowercase:
+    #f = open("Letters/" + letter+".txt", "r")
+    #a.append(f.read())
+    #    f.close()
 
-print(a)
+#print(a)
+
+import glob
+
+letters= []
+file_list = glob.glob("Letters/*.txt")
+
+for filename in file_list:
+    with open(filename, "r") as file:
+        letters.append(file.read().strip("\n"))
+
+
+print(letters)
